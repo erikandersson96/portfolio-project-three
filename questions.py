f5721b02ab16482289abba6670cf93fecf4b8a9f
erikandersson96 <erik96.andersson@gmail.com>
2022-04-03 09:30:08 +0000

    Add questions file and 12 questions in it

---
+++ questions.py
@@ -0,0 +1,72 @@
+formula_one_questions = [
+    {
+        "question": "The year is 2004, \
+            and the driver is Michael Schumacher. Name the team mate?:", 
+        "answers": ["Felipe Massa", "Rubens Barrichello"], 
+        "correct": "Rubens Barrichello"
+    }, 
+    {
+        "question": "Back to 1986 now. Who was Ayrton Senna's \
+            team mate at Team Lotus?:", 
+        "answers": ["Johnny Dumfries", "Mika Häkkinen"], 
+        "correct": "Johnny Dumfries"
+    }, 
+    {
+        "question": "Who was Max Verstappen's team mate at \
+            Toro Rosso in 2015? ", 
+        "answers": ["Danill Kvyat", "Carlos Sainz"], 
+        "correct": "Carlos Sainz"
+    }, 
+    {
+        "question": "Lewis Hamilton won his first Formula 1 \
+            World Championship in 2008, but by how many points?", 
+        "answers": ["3 points", "1 points"], 
+        "correct": "1 points"
+    }, 
+    {
+        "question": "Which one of these drivers was youngest when \
+            they clinched their second Formula 1 World Championship?", 
+        "answers": ["Sebastian Vettel", "Lewis Hamilton"], 
+        "correct": "Sebastian Vettel"
+    }, 
+    {
+        "question": "Hamilton took his first Grand Slam - pole position, \
+             win, and every lap led - in what Grand Prix of 2014?", 
+        "answers": ["Malaysia", "Monza"], 
+        "correct": "Malaysia"
+    }, 
+    {
+        "question": "At which Grand Prix did Hamilton secure the 2015 title?", 
+        "answers": ["USA", "Bahrain"], 
+        "correct": "USA"
+    }, 
+    {
+        "question": "Which driver suffered the most retirements (DNFs) \
+            in the 2020 season?", 
+        "answers": ["Kevin Magnussen", "Lance Stroll"], 
+        "correct": "Kevin Magnussen"
+    }, 
+    {
+        "question": "Which driver raced the most laps in 2020?", 
+        "answers": ["Lando Norris", "Lewis Hamilton"], 
+        "correct": "Lando Norris"
+    }, 
+    {
+        "question": "Which one of these races had the fewest \
+            classified finishers in 2020?", 
+        "answers": ["British Grand Prix", "Tuscan Grand Prix"], 
+        "correct": "Tuscan Grand Prix"
+    }, 
+    {
+        "question": "Daniel Ricciardo paid tribute to which of these \
+           sportspersons with a special helmet design in 2020?", 
+        "answers": ["Kobe Bryant", "Diego Maradona"], 
+        "correct": "Kobe Bryant" 
+    }, 
+    {
+        "question": "In which year was the first ever official \
+            formula one race?", 
+        "answers": ["1948", "1950"], 
+        "correct": "1950"
+    }
+]
