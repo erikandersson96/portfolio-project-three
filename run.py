d28a5b4b4510af2f01c1832a690b1ba1b29def09
erikandersson96 <erik96.andersson@gmail.com>
2022-04-03 09:36:02 +0000

    import questions and other modules to run.py

--- run.py
+++ run.py
@@ -1,3 +1,7 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+from questions import formula_one_questions
+import os 
+import time 
+import random 
