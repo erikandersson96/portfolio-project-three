4d166bb3316a2213ecc584714dcfde2c62e5d1e0
erikandersson96 <erik96.andersson@gmail.com>
2022-04-06 12:41:21 +0000

    Add user_question & correct_answer_question()

--- run.py
+++ run.py
@@ -119,7 +119,7 @@ def get_username():
 
 def start_random_quiz(): 
     """
-    Start the quiz and generate a random question from questions.py 
+    Start the quiz and generate a random question from formula_questions 
     """
     previous_question = []
     quiz_question = []
@@ -129,6 +129,46 @@ def start_random_quiz():
             previous_question.append(x)
             quiz_question.append(formula_questions[x])
     return quiz_question
+
+
+def user_question():
+    """
+    Displays the random generated question for the user 
+    """
+    i = 0 
+    point = 0 
+    while i < 12: 
+        print(quiz_question[i]["question"])
+        print(f"1 - {quiz_question[i]['options'][0]}")
+        print(f"2 - {quiz_question[i]['options'][1]}")
+        
+        correct_answer = correct_answer_question(quiz_question[i])
+        user_answer = user_answer_input()
+        if user_answer == correct_answer: 
+            point += 1 
+            print(f"Correct! Well done. You scored {point} points!")
+        else: 
+            print("Oh no you guessed wrong. Better luck in the next question!")
+        time.sleep(4)
+        i = i + 1 
+    return point 
+
+
+def correct_answer_question(quiz_question):
+    """
+    Checks for the correct answer out of the two options for each question 
+    """ 
+    if quiz_question['correct'] == quiz_question['options'][0]: 
+        return 1
+    elif quiz_question['correct'] == quiz_question['options'][1]: 
+        return 2
+
+
+
+
+
+
+
 
 
 
