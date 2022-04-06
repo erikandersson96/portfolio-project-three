2ffc3a8a82eac01fabf4b87252372a1b72ebe349
erikandersson96 <erik96.andersson@gmail.com>
2022-04-06 10:23:13 +0000

    Add main_meny_options function to run.py

--- run.py
+++ run.py
@@ -19,8 +19,7 @@ def game_start():
     """
     Start up the game, welcome message and start up the meny with options
     """
-    clear()
-    print("Hello there! Welcome to this Formula One Quiz!")
+    print("Hello player! Welcome to this Formula One Quiz!\n")
     f1_text = pyfiglet.figlet_format("F 1  Q U I Z", font = "3-d")
     print(f1_text)
     user_game_meny()
@@ -31,8 +30,39 @@ def user_game_meny():
     Game meny with the two options of either start the quiz or watch game 
     rules for the quiz
     """ 
-    print("Hello player! Are you ready for some Formula One questions?")
+    print("Are you ready for some Formula One questions?")
     print("""
     -        Start Quiz        -
-    -        Quiz Rules        -
-    Type 's or S' to start the quiz, type 'r or R' to watch Quiz Rules.""")
+    -           Rules          -
+
+    Type 's or S' to start the quiz, type 'r or R' to see the Rules.\n""")
+    main_meny_options()
+
+
+def main_meny_options(): 
+    """
+    Main meny to check which option the user selected between Start Quiz 
+    or Rules 
+    """
+    try: 
+        while True: 
+            option = input("")
+            option = option.upper()
+            if option not in ["S", "R"]: 
+                raise Exception
+            else: 
+                if option == 'S':
+                    print("Start Quiz")
+                    break 
+                elif option == 'R': 
+                    print("Rules")
+                    break
+    except Exception: 
+        clear()
+        print("""Wrong... You did not type 's or S' or 'r or R' to choose 
+        either Start Quiz or see the Rules. Please type the following to 
+        continue: 's or S' for Start Quiz or 'r or R' to see the Rules.\n""")
+        user_game_meny()
+
+
+game_start()
