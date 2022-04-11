e7fe7b7f1f984e5fb3757823b4b5c636b20e34c9
erikandersson96 <erik96.andersson@gmail.com>
2022-04-11 09:48:49 +0000

    Add 0.5 sec to delay how_many_questions run.py

--- run.py
+++ run.py
@@ -110,6 +110,7 @@ def get_username():
         while True:
             user_name = str(input("Write your username: \n"))
             if len(user_name) <= 14 and user_name.isalpha():
+                print("")
                 print(f"Thank you {user_name}!")
                 return user_name
             else:
@@ -123,8 +124,9 @@ def how_many_questions():
     """
     User get to choose to play either 6 or 12 questions
     """
-    print("Loading...")
-    time.sleep(2)
+    print("")
+    print("Loading game...")
+    time.sleep(2.5)
     clear()
     print("How many questions would you like to play?")
     while True:
