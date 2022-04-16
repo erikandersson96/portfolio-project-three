030c4efcd7c4a8e5a1834c8a3763e095c08809f5
erikandersson96 <erik96.andersson@gmail.com>
2022-04-16 07:32:37 +0000

    Fix spelling in questions.py

--- questions.py
+++ questions.py
@@ -1,72 +1,72 @@
 formula_questions = [
     {
         "question": "The year is 2004, \
-and the driver is Michael Schumacher. Name the team mate?:", 
-        "options": ["Felipe Massa", "Rubens Barrichello"], 
+and the driver is Michael Schumacher. Name the team mate?:",
+        "options": ["Felipe Massa", "Rubens Barrichello"],
         "correct": "Rubens Barrichello"
-    }, 
+    },
     {
         "question": "Back to 1986 now. Who was Ayrton Senna's \
-team mate at Team Lotus?:", 
-        "options": ["Johnny Dumfries", "Mika Häkkinen"], 
+team mate at Team Lotus?:",
+        "options": ["Johnny Dumfries", "Mika Häkkinen"],
         "correct": "Johnny Dumfries"
-    }, 
+    },
     {
         "question": "Who was Max Verstappen's team mate at \
-Toro Rosso in 2015? ", 
-        "options": ["Danill Kvyat", "Carlos Sainz"], 
+Toro Rosso in 2015? ",
+        "options": ["Danill Kvyat", "Carlos Sainz"],
         "correct": "Carlos Sainz"
-    }, 
+    },
     {
         "question": "Lewis Hamilton won his first Formula 1 \
-World Championship in 2008, but by how many points?", 
-        "options": ["3 points", "1 points"], 
+World Championship in 2008, but by how many points?",
+        "options": ["3 points", "1 points"],
         "correct": "1 points"
-    }, 
+    },
     {
         "question": "Which one of these drivers was youngest when \
-they clinched their second Formula 1 World Championship?", 
-        "options": ["Sebastian Vettel", "Lewis Hamilton"], 
+they clinched their second Formula 1 World Championship?",
+        "options": ["Sebastian Vettel", "Lewis Hamilton"],
         "correct": "Sebastian Vettel"
-    }, 
+    },
     {
-        "question": "Hamilton took his first Grand Slam - pole position, \
-win, and every lap led - in what Grand Prix of 2014?", 
-        "options": ["Malaysia", "Monza"], 
+        "question": "Hamilton took his first Grand Slam, pole position, \
+win, and every lap led. In what Grand Prix of 2014?",
+        "options": ["Malaysia", "Monza"],
         "correct": "Malaysia"
-    }, 
+    },
     {
-        "question": "At which Grand Prix did Hamilton secure the 2015 title?", 
-        "options": ["USA", "Bahrain"], 
+        "question": "At which Grand Prix did Hamilton secure the 2015 title?",
+        "options": ["USA", "Bahrain"],
         "correct": "USA"
-    }, 
+    },
     {
         "question": "Which driver suffered the most retirements (DNFs) \
-in the 2020 season?", 
-        "options": ["Kevin Magnussen", "Lance Stroll"], 
+in the 2020 season?",
+        "options": ["Kevin Magnussen", "Lance Stroll"],
         "correct": "Kevin Magnussen"
-    }, 
+    },
     {
-        "question": "Which driver raced the most laps in 2020?", 
-        "options": ["Lando Norris", "Lewis Hamilton"], 
+        "question": "Which driver raced the most laps in 2020?",
+        "options": ["Lando Norris", "Lewis Hamilton"],
         "correct": "Lando Norris"
-    }, 
+    },
     {
         "question": "Which one of these races had the fewest \
-classified finishers in 2020?", 
-        "options": ["British Grand Prix", "Tuscan Grand Prix"], 
+classified finishers in 2020?",
+        "options": ["British Grand Prix", "Tuscan Grand Prix"],
         "correct": "Tuscan Grand Prix"
-    }, 
+    },
     {
         "question": "Daniel Ricciardo paid tribute to which of these \
-sportspersons with a special helmet design in 2020?", 
-        "options": ["Kobe Bryant", "Diego Maradona"], 
-        "correct": "Kobe Bryant" 
-    }, 
+persons with a special helmet 2020?",
+        "options": ["Kobe Bryant", "Diego Maradona"],
+        "correct": "Kobe Bryant"
+    },
     {
         "question": "In which year was the first ever official \
-formula one race?", 
-        "options": ["1948", "1950"], 
+formula one race?",
+        "options": ["1948", "1950"],
         "correct": "1950"
     }
 ]
